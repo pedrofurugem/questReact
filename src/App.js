8d02167de6c1ff8e4242ef7146553f3ed31adde6
Pedro Roberto <pedrofuruguem@hotmail.com>
2022-10-28 19:03:02 -0300

    desafio1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import Text from './components/Text/Text.js'
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Text />
   );
 }
-
+//style={{color: 'blue'}}
 export default App;
+
+/*
+O nome do componente e o nome das props devem ser bem
+pensados, lembre-se de dar bons nomes que sejam fáceis de
+outras pessoas entenderem.
+
+1 - Dê um bom nome ao seu componente;
+2 - Faça o componente renderizar na tela um parágrafo colorido
+e com todas as letras maiúsculas usando JS;
+3 - Utilize props para passar o texto e a cor de texto desejada
+para dentro do componente (a cor pode ser uma palavra, como
+'red', ou um hexadecimal);
+4 - Pode utilizar o style inline para colorir o componente da forma
+que foi mostrado nas aulas;
+Se ficar com dúvidas revise as aulas.
+text-transform: uppercase
+*/
