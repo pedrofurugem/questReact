5d8344d482ea50dc615849813b6dbd64876fe0ea
Pedro Roberto <pedrofuruguem@hotmail.com>
2022-10-28 22:25:30 -0300

    finalizado

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,11 +3,11 @@ import './text.css'
 
 const paragraph = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-const Text = ({color, upper}) => {
+const Text = ({color}) => {
     
         return(
             <div className="paragrafo">
-                <p style={{ color: color, texttransform: upper }}
+                <p style={{ color: color }}
                 >{paragraph.toUpperCase()}</p>
             </div>
         )
